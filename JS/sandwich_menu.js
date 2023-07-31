f760a9d1a63f911885975cb198d0559abbfb655e
olegbucolo <90249582+olegbucolo@users.noreply.github.com>
2023-07-31 17:27:40 +0200

    first site
    html:
    index, work, about, login
    css:
    styles
    js:
    sandwich_menu, buttonsClick

---
+++ JS/sandwich_menu.js
@@ -0,0 +1,9 @@
+const headOuter = document.querySelector('.head-outer');
+const sandwichMenu = document.querySelector('.sandwich-menu');
+
+sandwichMenu.addEventListener('click', function (){
+        headOuter.classList.toggle('sandwiched');
+
+})
+
+sandwichMenu.addEventListener('')
