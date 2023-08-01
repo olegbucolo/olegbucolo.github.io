6151024d0ee86d091bc9aae6c159330841f2328f
olegbucolo <90249582+olegbucolo@users.noreply.github.com>
2023-08-01 18:01:22 +0200

    replacing 1st website with 2nd
    gone the orange website with confusing things and uncertain js giving the place to midjourney + html + css + 0.001% js

---
+++ JS/header_sandwich.js
@@ -0,0 +1,10 @@
+const headOuter = document.querySelector('.head-outer');
+const sandwich = document.querySelector('.nav-link-sandwich');
+
+
+sandwich.addEventListener('click', function(event){
+    // removing the ability of the <a class="nav-link-sandwich"><span>_</span><span>_</span></a> to redirect the page (therefore reloading and cancelling this script which adds a class to the .head-outer div element)
+    event.preventDefault();
+    // toggling the .pressed class onto headOuter in order to have our sandwich menu working
+    headOuter.classList.toggle('pressed');
+})
--- JS/sandwich_menu.js
+++
@@ -1,9 +0,0 @@
-const headOuter = document.querySelector('.head-outer');
-const sandwichMenu = document.querySelector('.sandwich-menu');
-
-sandwichMenu.addEventListener('click', function (){
-        headOuter.classList.toggle('sandwiched');
-
-})
-
-sandwichMenu.addEventListener('')
