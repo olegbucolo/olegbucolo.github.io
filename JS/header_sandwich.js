92a172dddf9aad486594a0f370f202700fb39ac2
olegbucolo <90249582+olegbucolo@users.noreply.github.com>
2023-08-02 15:20:09 +0200

    grid, grid animation
    polishing the grid and adding an animation to the grid

--- JS/header_sandwich.js
+++ JS/header_sandwich.js
@@ -6,8 +6,7 @@ sandwich.addEventListener('click', function(event){
     // removing the ability of the <a class="nav-link-sandwich"><span>_</span><span>_</span></a> to redirect the page (therefore reloading and cancelling this script which adds a class to the .head-outer div element)
     event.preventDefault();
     // toggling the .pressed class onto headOuter in order to have our sandwich menu working
-        headOuter.classList.toggle('pressed');
-    
+    headOuter.classList.toggle('pressed');
     // adds the class .no-scroll to set a overflow: hidden to the body when the menu is opened
     document.body.classList.toggle('no-scroll');
 })
