cfe2c5f957fe0e75ad91dc699282cbd061c6a05d
olegbucolo <90249582+olegbucolo@users.noreply.github.com>
2023-08-05 15:04:45 +0200

    continuing home
    added second section 'why choose us' and 'our process', to polish and add bknd

--- JS/parallax-img.js
+++ JS/parallax-img.js
@@ -1,9 +1,14 @@
+// IMG 01 JS
+
 const img01 = document.querySelector('.img01');
 const img01Styles = window.getComputedStyle(img01);
 const img01Bottom = img01Styles.getPropertyValue('bottom');
 console.log('the bottom of the img01 is: ' + img01Bottom);
 const img01BottomParsed = removePx(img01Bottom);
 console.log('the bottom of the img01 WITHOUT PX is: ' + img01BottomParsed);
+
+// IMG 02 JS
+
 
 const img02 = document.querySelector('.img02');
 const img02Styles = window.getComputedStyle(img02);
@@ -16,6 +21,11 @@ console.log('the bottom of the img02 WITHOUT PX is: ' + img02BottomParsed);
 
 window.addEventListener('scroll', function(){
     let s = window.scrollY;
+    let h = window.innerHeight;
+    // used to add the innerHeight(the total height of the 
+    // current viewport + the height of the top or bottom 
+    //of the element we want to parallax scroll )
+    // let hT = h + img03BottomParsed;
     
     const img01AdjustedBottom = (img01BottomParsed + s * 1.8) + 'px';
     img01.style.setProperty('bottom', img01AdjustedBottom);
